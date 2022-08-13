6234da337db08bfaa429633bd5ea9cb652d9d455
charin <charin.aewzipo@gmail.com>
2022-08-14 01:12:37 +0700

    make better

--- server.js
+++ server.js
@@ -20,13 +20,20 @@ const connect = () => {
     });
 };
 
-const corsOptions = {
-  origin: "*",
-  optionsSuccessStatus: 200,
-};
-app.use(cors(corsOptions));
-app.options("*", cors());
-
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+//Cors Configuration - End
 app.use(express.json());
 app.use(cookieParser());
 
