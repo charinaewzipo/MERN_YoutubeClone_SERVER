ee25096933ffed5f4dba5eb818ce03aaeaf2aab4
charin <charin.aewzipo@gmail.com>
2022-08-14 00:35:41 +0700

    make it better

--- server.js
+++ server.js
@@ -24,16 +24,8 @@ const whitelist = [
   "http://localhost:3000",
   "https://client-youtubeclone.herokuapp.com/",
 ];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || whitelist.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-  credentials: true,
-};
+corsOptions = { origin: "https://client-youtubeclone.herokuapp.com" };
+
 app.use(cors(corsOptions));
 
 app.use(express.json());
