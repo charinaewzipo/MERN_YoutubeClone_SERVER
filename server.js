46453ace9527ecadec191476e39eee6a2273b195
charin <charin.aewzipo@gmail.com>
2022-08-14 15:14:20 +0700

    revmoe cors manual

--- server.js
+++ server.js
@@ -19,22 +19,22 @@ const connect = () => {
       throw err;
     });
 };
-
-//Cors Configuration - Start
+app.use(cors());
 app.use(express.json());
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested, Content-Type, Accept Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
-    return res.status(200).json({});
-  }
-  next();
-});
-//Cors Configuration - End
+// //Cors Configuration - Start
+// app.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested, Content-Type, Accept Authorization"
+//   );
+//   if (req.method === "OPTIONS") {
+//     res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+//     return res.status(200).json({});
+//   }
+//   next();
+// });
+// //Cors Configuration - End
 app.use(cookieParser());
 
 app.use("/api/users", userRoutes);
