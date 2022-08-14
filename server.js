781bd3b9eff992d91facdf730de3b4f4a356427e
charin <charin.aewzipo@gmail.com>
2022-08-14 22:08:29 +0700

    make better

--- server.js
+++ server.js
@@ -28,7 +28,11 @@ const connect = () => {
 app.use(function (req, res, next) {
   res.header("Content-Type", "application/json;charset=UTF-8");
   res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header(
+    "Access-Control-Allow-Origin",
+    "http://localhost:3000",
+    "https://client-youtubeclone.herokuapp.com/"
+  );
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
