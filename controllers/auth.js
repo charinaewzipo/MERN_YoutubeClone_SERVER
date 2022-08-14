31afa77e270e1d5851adb52f3a70abdf95ec55e9
charin <charin.aewzipo@gmail.com>
2022-08-14 22:33:41 +0700

    make better

--- controllers/auth.js
+++ controllers/auth.js
@@ -33,6 +33,8 @@ export const signin = async (req, res, next) => {
     res
       .cookie("access_token", token, {
         httpOnly: true, // try this
+        sameSite: "none",
+        secure: true,
       })
       .status(200)
       .json(others);
